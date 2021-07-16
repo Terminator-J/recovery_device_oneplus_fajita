600d521aedc12e7bab14b41b7dd13acad57a0f22
nebrassy <nebras30@gmail.com>
2021-07-16 23:13:01 +0200

    fajita: TWRP
    partially based on official TeamWin tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fajita.mk
+    $(LOCAL_DIR)/twrp_fajita.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fajita-user \
-    lineage_fajita-userdebug \
-    lineage_fajita-eng
+    twrp_fajita-user \
+    twrp_fajita-userdebug \
+    twrp_fajita-eng
